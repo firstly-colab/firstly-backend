df22f93d83a410b0db1846b4e6a8373bb749091e
Gurupriyan <gurupriyan98@gmail.com>
2022-08-02 15:11:08 -0400

    Create the result route

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const pool = require('./dbConfig')
 const app = express()
 const PORT = process.env.PORT
 
-app.get('/', (req, res) => {
-    res.status(200).json('hello')
+app.get('/result', async (req, res) => {
+    const resp = await pool.query('SELECT * FROM public.users JOIN public.feedback ON users.id = feedback.user_id JOIN public.response ON feedback.response_id = response.id WHERE (response.category = $1 OR response.category = $2 OR response.category = $3) AND feedback.desired = $4', ['pet', 'travel', 'food', true]).then(result => result.rows)
+    res.status(200).json(resp)
 })
 
 app.listen(PORT, () => {
