64e4628dbceefaecaa4a3b1997c6e65e2f91d240
Gurupriyan <gurupriyan98@gmail.com>
2022-08-12 09:56:10 -0400

    added functionality for users that are not logged in

--- index.js
+++ index.js
@@ -19,10 +19,23 @@ app.get('/', (req, res) => {
 app.use(AuthRouter)
 
 
-app.get('/result', async (req, res) =>   {
-    const resp = await pool.query('SELECT * FROM public.user JOIN public.feedback ON public.user.id = feedback.user_id JOIN public.response ON feedback.response_id = response.id WHERE (response.category = $1 OR response.category = $2 OR response.category = $3) AND feedback.desired = $4', ['pet', 'travel', 'food', true]).then(result => result.rows)
-    res.status(200).json(resp)
+// app.post('/result', async (req, res) =>   {
+//     const {checked} = req.body
+//     console.log(checked)
+//     const resp = await pool.query('SELECT * FROM public.user JOIN public.feedback ON public.user.id = feedback.user_id JOIN public.response ON feedback.response_id = response.id WHERE (response.category = $1 OR response.category = $2 OR response.category = $3) AND feedback.desired = $4', ['pet', 'travel', 'food', true]).then(result => result.rows)
+//     res.status(200).json({resp})
+// })
+app.post('/result', async (req, res) =>   {
+    const {checked} = req.body
+    let resp = []
+    checked.forEach(async (ele, index) => {
+        const data = await pool.query('SELECT * FROM public.response WHERE public.response.category = $1', [ele]).then(res => res.rows)
+        resp = [...resp, ...data]
+        if (index === checked.length - 1) res.status(200).json(resp)
+    })
 })
+
+
 
 app.listen(PORT, () => {
     console.log("Listening to ", PORT)
--- seeds/01_response_seed.js
+++ seeds/01_response_seed.js
@@ -6,8 +6,17 @@
   // Deletes ALL existing entries
   await knex('response').del()
   await knex('response').insert([
-    {dialogue: 'If you can eat one meal for the rest of your life, what would it be and why?', category: 'food'},
-    {dialogue: 'If you are on vacation, what is the first thing you like to do when you arrive in a new place?', category: 'travel'},
-    {dialogue: 'How would you describe your pet personality', category: 'pet'}
+    {dialogue: 'If you can eat one meal for the rest of your life, what would it be and why?', category: 'Foodie'},
+    {dialogue: 'If you are on vacation, what is the first thing you like to do when you arrive in a new place?', category: 'Loves to travel'},
+    {dialogue: 'How would you describe your pet personality', category: 'Pet lover'},
+    {dialogue: 'Lorem Ipsum FRIENDS', category: 'Friends oriented'},
+    {dialogue: 'Lorem Ipsum OUTDOOR', category: 'Outdoorsy'},
+    {dialogue: 'Lorem Ipsum HUMOR', category: 'Humorous'},
+    {dialogue: 'Lorem Ipsum FAMILY', category: 'Family oriented'},
+    {dialogue: 'Lorem Ipsum HOBBY', category: 'Passionate about personal hobbies'},
+    {dialogue: 'Lorem Ipsum NEW THINGS', category: 'Open to trying new things'},
+    {dialogue: 'Lorem Ipsum MUSIC', category: 'Music lover'},
+    {dialogue: 'Lorem Ipsum NETFLIX', category: 'Netflix & Chill-er'},
+    {dialogue: 'Lorem Ipsum HIKE', category: 'Planning the next hike'},
   ]);
 };
