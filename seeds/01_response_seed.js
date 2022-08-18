0c2ca601de2b1d8f1a2559a0b1dc14237693478a
Gurupriyan <gurupriyan98@gmail.com>
2022-08-18 14:08:05 -0400

    replaced lorem ipsum with actual conversation starters

--- seeds/01_response_seed.js
+++ seeds/01_response_seed.js
@@ -6,17 +6,19 @@
   // Deletes ALL existing entries
   await knex('response').del()
   await knex('response').insert([
-    {dialogue: 'If you can eat one meal for the rest of your life, what would it be and why?', category: 'Foodie'},
-    {dialogue: 'If you are on vacation, what is the first thing you like to do when you arrive in a new place?', category: 'Loves to travel'},
-    {dialogue: 'How would you describe your pet personality', category: 'Pet lover'},
-    {dialogue: 'Lorem Ipsum FRIENDS', category: 'Friends oriented'},
-    {dialogue: 'Lorem Ipsum OUTDOOR', category: 'Outdoorsy'},
-    {dialogue: 'Lorem Ipsum HUMOR', category: 'Humorous'},
-    {dialogue: 'Lorem Ipsum FAMILY', category: 'Family oriented'},
-    {dialogue: 'Lorem Ipsum HOBBY', category: 'Passionate about personal hobbies'},
-    {dialogue: 'Lorem Ipsum NEW THINGS', category: 'Open to trying new things'},
-    {dialogue: 'Lorem Ipsum MUSIC', category: 'Music lover'},
-    {dialogue: 'Lorem Ipsum NETFLIX', category: 'Netflix & Chill-er'},
-    {dialogue: 'Lorem Ipsum HIKE', category: 'Planning the next hike'},
+    {dialogue: "If you can eat one meal for the rest of your life, what would it be and why?", category: 'Foodie'},
+    {dialogue: "If you are on vacation, what is the first thing you like to do when you arrive in a new place?", category: 'Loves to travel'},
+    {dialogue: "How would you describe your pet personality?", category: 'Pet lover'},
+    {dialogue: "Which friend would you most likely have to bail out from jail?", category: 'Friends oriented'},
+    {dialogue: "What's the craziest memory you have with your best friend(s)?", category: 'Friends oriented'},
+    {dialogue: 'What would you rather: mountains, forest, ocean? Why?', category: 'Outdoorsy'},
+    {dialogue: "What's the most embarrassing way you've been injured?", category: 'Humorous'},
+    {dialogue: "What's your most useless talent?", category: 'Humorous'},
+    {dialogue: "What's the best advice you've ever received from a family member?", category: 'Family oriented'},
+    {dialogue: 'Are you working on any personal passion projects?', category: 'Passionate about personal hobbies'},
+    {dialogue: "What's something you've always been curious to try but haven't yet?", category: 'Open to trying new things'},
+    {dialogue: 'What would be your 3 go-to songs for a karaoke night?', category: 'Music lover'},
+    {dialogue: 'If you can spend a whole day with a character from your favorite TV show, who would it be and why? ', category: 'Netflix & Chill-er'},
+    {dialogue: "What was the hardest hike you did that you're most proud of?", category: 'Planning the next hike'},
   ]);
 };
