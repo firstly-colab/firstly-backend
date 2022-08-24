2e4cde74929dbd7440c748003bdd85d020c75f24
Gurupriyan <gurupriyan98@gmail.com>
2022-08-23 23:28:47 -0400

    changed dislike to a delete request

--- src/routes/likedQues.js
+++ src/routes/likedQues.js
@@ -4,7 +4,7 @@ const LikedQuesController = require('../controllers/likedQuesController')
 
 router.get('/liked-question/:user_id', LikedQuesController.getLiked)
 router.post('/liked-question', LikedQuesController.sendLiked)
-router.post('/liked-question', LikedQuesController.sendDisliked)
+router.delete('/liked-question', LikedQuesController.sendDisliked)
 
 
 module.exports = router
